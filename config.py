9dbe8ac32eb468cf3bad66dc5826c43e24a7ffd5
denki307 <200189507+denki307@users.noreply.github.com>
2026-02-06 07:14:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "27383453"))
-API_HASH = getenv("API_HASH", "4c246fb0c649477cc2e79b6a178ddfaa")
+API_ID = int(getenv("API_ID", "16490688"))
+API_HASH = getenv("API_HASH", "ad958ab159ed1125afb5fcdc3875b120")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
-OWNER_ID = int(getenv("OWNER_ID", "6762113050"))
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", "ITSZSHUKLA")
+OWNER_ID = int(getenv("OWNER_ID", "7252249791"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ftyvfbgubhu7:hDZwwlNzlKBzls84@ameliamusicbot.f7dzw.mongodb.net/?retryWrites=true&w=majority&appName=AmeliaMusicbot")
+MUST_JOIN = getenv("MUST_JOIN", "MUSIC_BOT_WORLD")
 START_IMG = getenv("START_IMG", "https://files.catbox.moe/520y6h.jpg")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "MASTIWITHFRIENDSXD")
-UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "SHIVANSH474")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "MUSIC_BOT_TEAM")
+UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "MUSIC_BOT_WORLD")
