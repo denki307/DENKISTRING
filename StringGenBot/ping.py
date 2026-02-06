cb8b5504d1b0aaefa28fdaacb65a4678aa37e589
denki307 <200189507+denki307@users.noreply.github.com>
2026-02-06 07:34:24 +0530

    Update ping.py

--- StringGenBot/ping.py
+++ StringGenBot/ping.py
@@ -45,7 +45,7 @@ async def ping_handler(client: Client, message: Message):
 ➥ ᴘᴏɴɢ: `{latency:.3f} ms`
 ➥ ᴜᴘᴛɪᴍᴇ: {uptime}
 
-⦿ ᴄʀᴇᴀᴛᴇᴅ ʙʏ [sʜɪᴠᴀɴsʜ-xᴅ](https://t.me/ITSZ_SHIVANSH)**""",
+⦿ ᴄʀᴇᴀᴛᴇᴅ ʙʏ [𝐃𝐄𝐍𝐊𝐈](https://t.me/DENKI1234)**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
